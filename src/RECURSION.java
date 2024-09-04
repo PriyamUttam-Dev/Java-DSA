90964bdc8d273ef5ba56cfb094a962f9319ac6ec
Priyam Uttam <priyamuttam.dev@gmail.com>
2024-09-04 21:55:31 +0530

    File Name Changed

--- src/RECURSION.java
+++ src/RECURSION.java
@@ -1,7 +1,16 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class RECURSION {
+    public static void print(int n){
+        if(n == 0 ){
+            return;
+        }
+        else{
+            print(n-1);
+            System.out.println("Hello World"+n);
+        }
+    }
     public static void main(String[] args) {
-
+     print(5);
     }
 }
